a2d5f5aa33ae85f0751012513ef8419765ed4e2e
Jesse <akohgodwinjesse@gmail.com>
2022-07-04 12:11:10 +0100

    Update  nullResponsive.vue

--- src/components/ nullResponsive.vue
+++ src/components/ nullResponsive.vue
@@ -3,6 +3,8 @@
         <div class="null_container">
             <h2>
                 Black is the new white
+                <br>
+                ~ Too Tired for a responsive build
             </h2>
         </div>
     </div>
@@ -16,12 +18,21 @@
     position: fixed;
     height: 100vh;
     z-index: 990;
-    @include flex(center, center, 0);
-    @include flexcol(2rem);
     background-color: black;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
+    .null_container{
+        height: 100%;
+        width: 100%;
+           @include flex(center, center, 0);
+           @include flexcol(2rem);
+        h2{
+            font-size: 3em;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            color: white;
+        }
+    }
 }
 </style>
