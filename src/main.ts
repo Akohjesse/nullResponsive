1856a43e888c55ff8ae017b0ceef145a4da82983
Jesse <akohgodwinjesse@gmail.com>
2022-07-05 10:27:56 +0100

    rename

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import nullResponsive from "./nullResponsive";
-const nullconfig ={
-  inputText: "input text",
-  breakpoint: 1000,
-  bg_color: "purple",
-  text_color: "white",
-};
 
-createApp(App).use(nullResponsive, nullconfig).mount("#app");
+createApp(App).use(nullResponsive).mount("#app");
